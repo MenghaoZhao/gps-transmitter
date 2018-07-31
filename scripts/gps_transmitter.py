584dc96df9608105348153bf7a49499ac191944e
root <root@exciting23.sd.tusimple.ai>
2018-07-31 02:09:16 +0000

    use middleware

--- scripts/gps_transmitter.py
+++ scripts/gps_transmitter.py
@@ -2,24 +2,28 @@
 
 import rospy
 from std_msgs.msg import String
-# middleware as mw
-#from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
-#    init_node, spin, start_heartbeat
+import middleware as mw
+from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
+    init_node, spin, start_heartbeat
 from ts_rpc.client import RemoteObject
+import time
 
 class gps_transmitter():
 
     def __init__(self):
-        gps_sub = rospy.Subscriber("novatel_gps", String, self._callback)
+	gps_sub = rospy.Subscriber('novatel_gps', String, self._callback)
+	#gps_sub.add_callback(self._callback)
+	print('call back added')
         self.vm = RemoteObject('http://0.0.0.0:5023')
 
     def _callback(self, data):
         latitude = str(data).split('latitude:')[1].split("\\n")[0]
         longitude = str(data).split('longitude:')[1].split("\\n")[0]
        # print(str(data))
+	print('in call back')
         print(latitude)
         print(longitude)
-        self.vm.vehicle.set_gps('Octopus-B1', [float(latitude), float(longitude)])
+        self.vm.vehicle.set_gps('Octopus-B1', [float(time.time())])
 
 
 if __name__ == '__main__':
