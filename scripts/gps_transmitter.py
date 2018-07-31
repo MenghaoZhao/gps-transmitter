4b8971c4342cd07c5dc752f882ac706ebeff94de
root <root@exciting23.sd.tusimple.ai>
2018-07-31 21:18:07 +0000

    add middleware

--- scripts/gps_transmitter.py
+++ scripts/gps_transmitter.py
@@ -2,28 +2,32 @@
 
 import rospy
 from std_msgs.msg import String
-# middleware as mw
-#from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
-#    init_node, spin, start_heartbeat
+from novatel_msgs.msg import *
+import middleware as mw
+from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
+    init_node, spin, start_heartbeat
 from ts_rpc.client import RemoteObject
+import time
 
 class gps_transmitter():
 
     def __init__(self):
-        gps_sub = rospy.Subscriber("novatel_gps", String, self._callback)
-        self.vm = RemoteObject('http://0.0.0.0:5023')
+        gps_sub = Subscriber("novatel_data/inspvax", INSPVAX)
+        #gps_sub_ros = rospy.Subscriber("novatel_gps", String, self._callback)
+	gps_sub.add_callback(self._callback)
+	self.vm = RemoteObject('http://0.0.0.0:5023')
 
     def _callback(self, data):
-        latitude = str(data).split('latitude:')[1].split("\\n")[0]
-        longitude = str(data).split('longitude:')[1].split("\\n")[0]
+        latitude = data['novatel_data/inspvax'].latitude
+        longitude = data['novatel_data/inspvax'].longitude
        # print(str(data))
         print(latitude)
         print(longitude)
-        self.vm.vehicle.set_gps('Octopus-B1', [float(latitude), float(longitude)])
+        self.vm.vehicle.set_gps('Octopus-B1', [latitude, longitude])
 
 
 if __name__ == '__main__':
-    rospy.init_node('gps_transmitter')
-    #start_heartbeat()
+    init_node('gps_transmitter')
+    start_heartbeat()
     g1 = gps_transmitter()
-    rospy.spin()
+    spin()
