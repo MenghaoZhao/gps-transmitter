6f4780c83fe58cf0b1db5492b35d91f0ae8064a3
Menghao Zhao <menghao.zhao@exciting23.sd.tusimple.ai>
2018-07-30 17:41:56 -0700

    add set gps for vm in call back

--- scripts/gps_transmitter.py
+++ scripts/gps_transmitter.py
@@ -2,21 +2,28 @@
 
 import rospy
 from std_msgs.msg import String
-import middleware as mw
-from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
-    init_node, spin, start_heartbeat
+# middleware as mw
+#from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
+#    init_node, spin, start_heartbeat
+from ts_rpc.client import RemoteObject
 
 class gps_transmitter():
 
     def __init__(self):
         gps_sub = rospy.Subscriber("novatel_gps", String, self._callback)
+        self.vm = RemoteObject('http://0.0.0.0:5023')
 
     def _callback(self, data):
-        print(data)
+        latitude = str(data).split('latitude:')[1].split("\\n")[0]
+        longitude = str(data).split('longitude:')[1].split("\\n")[0]
+       # print(str(data))
+        print(latitude)
+        print(longitude)
+        self.vm.vehicle.set_gps('Octopus-B1', [float(latitude), float(longitude)])
 
 
 if __name__ == '__main__':
-    init_node('gps_transmitter')
-    start_heartbeat()
-    gps_transmitter()
-    spin()
+    rospy.init_node('gps_transmitter')
+    #start_heartbeat()
+    g1 = gps_transmitter()
+    rospy.spin()
