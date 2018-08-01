301a24e21c5140218b045c04ea70299addbdb4d4
root <root@exciting23.sd.tusimple.ai>
2018-08-01 19:14:35 +0000

    change subscribe data format and topic

--- scripts/gps_transmitter.py
+++ scripts/gps_transmitter.py
@@ -6,23 +6,19 @@ from novatel_msgs.msg import *
 import middleware as mw
 from middleware import Publisher, Subscriber, SmartMixer, Timer, run_handler_async, Duration, FunctionMonitor, \
     init_node, spin, start_heartbeat
-from ts_rpc.client import RemoteObject
+from ts_rpc.client import RemoteObject, find_rpc_service
 import time
 
 class gps_transmitter():
 
     def __init__(self):
-        gps_sub = Subscriber("novatel_data/inspvax", INSPVAX)
-        #gps_sub_ros = rospy.Subscriber("novatel_gps", String, self._callback)
-	gps_sub.add_callback(self._callback)
-	self.vm = RemoteObject('http://0.0.0.0:5023')
+        gps_sub = Subscriber("/novatel_data/inspvax", INSPVAX)
+        gps_sub.add_callback(self._callback)
+	self.vm = find_rpc_service('vm')
 
     def _callback(self, data):
-        latitude = data['novatel_data/inspvax'].latitude
-        longitude = data['novatel_data/inspvax'].longitude
-       # print(str(data))
-        print(latitude)
-        print(longitude)
+        latitude = data['/novatel_data/inspvax'].latitude
+        longitude = data['/novatel_data/inspvax'].longitude
         self.vm.vehicle.set_gps('Octopus-B1', [latitude, longitude])
 
 
